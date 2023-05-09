59665669b4d163350af3301cda75da56b680e262
MatchaBoy <soswald89@googlemail.com>
2023-05-09 07:32:44 +0200

    change

---
+++ Projekt_06_JavaScript_Mini-Übungen/Responsive Menu/main.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.getElementById('menu').classList.add('show-overlay-menu');
+}
+
+function closeMenu() {
+    document.getElementById('menu').classList.remove('show-overlay-menu');
+}
